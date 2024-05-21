4b84aea520aa0db232303038a3b75deb3a32aa96
Dhanush-varddhan-k <114069009+Dhanush-varddhan-k@users.noreply.github.com>
2024-05-21 21:31:28 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return  <footer className="py-1">
+                <p className="text-center mt-1 ">
+                    Chor bazzar, All Rights Reserved
+                </p>
+            </footer>
+}
